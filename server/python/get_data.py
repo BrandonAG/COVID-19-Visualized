35d71632a900706c6704b0e560a06b42e87f5623
Patrick Lee <patrickmklee93@gmail.com>
2021-08-16 02:00:35 -0700

    Filter dataframe to key statistics

--- server/python/get_data.py
+++ server/python/get_data.py
@@ -5,38 +5,39 @@ import pandas as pd
 # current_date = format(date.today(), "%m-%d-%y")
 def daily_report(report_date):
     formatted_date = report_date.format("%m-%d-%y")
-    url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/3eaccd9f5ac8f0ab66b03e43e0bfdb8bbf8cc947/csse_covid_19_data/csse_covid_19_daily_reports/' + formatted_date +'.csv'
+    url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/' + formatted_date +'.csv'
     serverdir = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
     tmpdir = os.path.join(serverdir, '_tmp')
+    
     if ( not os.path.isdir(tmpdir)):
         os.mkdir(tmpdir)
     tmpcsv = os.path.join(tmpdir,'tmp.csv')
     tmpjson = os.path.join(tmpdir,'tmp.json')
     print(url)
     r = requests.get(url)
+    if (r.status_code != 200):
+        raise Exception('404')
+        return 0
     print(r.status_code)
-    text= r.text
+    text = r.text
     with open(tmpcsv, 'w', newline='') as f:
         f.write(text)
-        #writer = csv.writer(f)
-        #writer.writerows(rows)
+        f.close()
+        # writer = csv.writer(f)
+        # writer.writerows(rows)
     data = dict()
     data[-1] = []
     with open(tmpcsv,'r') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             if (row['Country_Region'] == 'US'):
-                fips = row['FIPS']
                 value = dict(row.items())
-                if (fips == ''):
-                    key = -1 #data[-1].append(row.items())
-                else:
-                    key = int(row['FIPS'])
+                key = row['FIPS']
                 # value = dict(row.items())
                 # del value['FIPS']
                 data[key]= value
                 # print(row['FIPS'], row['Admin2'], row['Province_State'], row['Deaths'])
-    jsonData = json.dumps(data, sort_keys=True)
+    jsonData = json.dumps(data)
     with open(tmpjson,'w', newline='') as jsonfile:
         jsonfile.write(jsonData)
     #df = pd.DataFrame(jsonData,columns=['Deaths', 'Province_State', 'Admin2'])
@@ -47,4 +48,4 @@ if __name__ == '__main__':
     today = date.today()
     yesterday = format(today-timedelta(days=1),"%m-%d-%Y")
     df = daily_report(yesterday)
-    #df = pd.DataFrame.from_dict(data,orient='index')
+    
--- server/python/page.py
+++ server/python/page.py
@@ -5,39 +5,57 @@ from django.conf import settings
 from datetime import date, timedelta
 import pandas as pd
 from get_data import daily_report
+import django
 
-today = date.today()
-yesterday = format(today-timedelta(days=1),"%m-%d-%Y")
-cdf = daily_report(yesterday)
+settings.configure()
+django.setup()
+template = """<html>
+<head>
+<title>Template {{ target_date }}
+</title>
+</head>
+<body>
+<table>
+<thead>
+{% for k in data.keys %}
+<th>{{k}}</th>
+{% endfor %}
+</thead>
+<tbody>
+{% for k,v in data.items %}
+<tr>
+<th>{{v}}</th>
+<td>{{v}}</td>
+</tr>
+</tbody>
+{% endfor %}
 
-# df = pd.DataFrame(data.items(),columns=['Province_State','Confirmed','Deaths','Active'])
-# from django.template import engines
-# settings.configure(TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [],
-#         'APP_DIRS': False,
-#         'OPTIONS': {
-#             # ... some options here ...
-#         },
-#     },
-# ]) # We have to do this to use django templates standalone - see
-# # # http://stackoverflow.com/questions/98135/how-do-i-use-django-templates-without-the-rest-of-django
 
-# # Our template. Could just as easily be stored in a separate file
-# template = Template("My name is {{ my_name }}.")
-#  """
-# <html>
-# <head>
-# <title>Template {{ title }}</title>
-# </head>
-# <body>
-# Body with {{ mystring }}.
-# </body>
-# </html>
-# """
+</table>
+</body>
+</html>
+"""
 def doRender(dat):
-# c = Context()
+    # c = Context()
     return template.render(Context(dat))
-# doRender({"title": "title from code", "mystring":"string from code", "my_name":"Patrick"})
 
+# def main():
+today = date.today()
+for i in range(7):
+    try:
+        target_date = format(today-timedelta(days=i),"%m-%d-%Y")
+        daily = daily_report(target_date)
+    except Exception as e:
+        print(e.args)
+        if ('404' in e.args):
+            continue
+        else:
+            break
+    else:
+        break
+
+usDaily = daily.query('Country_Region == "US"')
+di = pd.Index(daily.FIPS)
+
+df = pd.DataFrame(usDaily.filter(['FIPS','Admin2','Province_State','Confirmed','Deaths','Case_Fatality_Ratio']).sort_values('Confirmed', ascending=False))
+print(df)
