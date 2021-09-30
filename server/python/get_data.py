59ce34a74d51738c6891e5a69b684a8209144c0e
Patrick Lee <patrickmklee93@gmail.com>
2021-09-29 19:47:06 -0700

    Add graph display

--- server/python/get_data.py
+++ server/python/get_data.py
@@ -1,18 +1,54 @@
 import requests
 from datetime import date, timedelta
+import re
 import os, csv, json
 import pandas as pd
+import pycountry
+SERVER_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+TMP_DIR = os.path.join(SERVER_DIR, '_tmp')
+TMP_CSV = os.path.join(TMP_DIR, 'tmp.csv')
 # current_date = format(date.today(), "%m-%d-%y")
+def download_csv(url,output_file=TMP_CSV):
+    if ( not os.path.isdir(TMP_DIR)):
+        os.mkdir(TMP_DIR)
+    print(url) 
+    r = requests.get(url)
+    if (r.status_code != 200):
+        raise Exception('404')
+    else:
+        text = r.text
+        with open(output_file, 'w', encoding='utf-8') as f:
+            f.write(text)
+    return 1
+def daily_report_us(report_date):
+    formatted_date = report_date.format("%m-%d-%y")
+    url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports_us/' + formatted_date +'.csv'
+    csv_output = os.path.join(TMP_DIR, '{}.csv'.format(formatted_date))
+    if (os.path.isfile(csv_output)):
+        print('File {} is already downloaded'.format(csv_output))
+    else:
+        download_csv(url,output_file=csv_output)
+    
+    tmpjson = os.path.join(TMP_DIR,'{}.json'.format(formatted_date))
+    data = {}
+    print('Reading from csv')
+    with open(csv_output,'r', encoding='utf-8', newline='\n') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for rows in reader:
+            key=rows['FIPS']
+            data[key] = rows
+    with open(tmpjson,'w', newline='') as jsonfile:
+        jsonfile.write(json.dumps(data,indent=4,sort_keys=True))
+    return pd.DataFrame(pd.read_csv(csv_output))
+    # print(r.status_code)
+    
 def daily_report(report_date):
     formatted_date = report_date.format("%m-%d-%y")
     url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/' + formatted_date +'.csv'
-    serverdir = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
-    tmpdir = os.path.join(serverdir, '_tmp')
-    
-    if ( not os.path.isdir(tmpdir)):
-        os.mkdir(tmpdir)
-    tmpcsv = os.path.join(tmpdir,'tmp.csv')
-    tmpjson = os.path.join(tmpdir,'tmp.json')
+    if ( not os.path.isdir(TMP_DIR)):
+        os.mkdir(TMP_DIR)
+    tmpcsv = os.path.join(TMP_DIR,'tmp.csv')
+    tmpjson = os.path.join(TMP_DIR,'tmp.json')
     print(url)
     r = requests.get(url)
     if (r.status_code != 200):
@@ -20,32 +56,64 @@ def daily_report(report_date):
         return 0
     print(r.status_code)
     text = r.text
-    with open(tmpcsv, 'w', newline='') as f:
+    with open(tmpcsv, 'w', encoding='utf-8', newline='\n') as f:
         f.write(text)
         f.close()
         # writer = csv.writer(f)
         # writer.writerows(rows)
-    data = dict()
-    data[-1] = []
-    with open(tmpcsv,'r') as csvfile:
+    data = {}
+    # data[-1] = []
+    j=-1
+    with open(tmpcsv,'r', encoding='utf-8', newline='\n') as csvfile:
         reader = csv.DictReader(csvfile)
-        for row in reader:
-            if (row['Country_Region'] == 'US'):
-                value = dict(row.items())
-                key = row['FIPS']
-                # value = dict(row.items())
-                # del value['FIPS']
-                data[key]= value
-                # print(row['FIPS'], row['Admin2'], row['Province_State'], row['Deaths'])
-    jsonData = json.dumps(data)
+        for rows in reader:
+            # if (row['Country_Region'] == 'US'):
+            # value = dict(row.items())
+            fips = rows['FIPS']
+            if (fips != ''):
+                key=int(fips)
+                try:
+                    (state_code, county_code) = re.search('^([\d]{,2})?([\d]{3})$', rows['FIPS']).groups()
+                except:
+                    print(fips, rows)
+                    state_code = fips
+                    county_code = 0
+                rows.update( {'state_code':state_code, 'county_code': county_code} )
+            else:
+                #except ValueError:
+                #print(rows['Country_Region'])
+
+                try:
+                    country = pycountry.countries.get(name=rows['Country_Region'])
+                    if (country is None):
+                        country = pycountry.countries.search_fuzzy(rows['Country_Region'])[0]
+                    key = int(country.numeric)
+                except (LookupError, AttributeError):
+                    print('Unknown country:  {}'.format(rows['Country_Region']))
+                    key = j
+                    j -= 1
+
+                # x =input('?')
+                # print(rows)
+                # x = input(' ')
+                # key = j
+                # j+=1
+            # value = dict(row.items())
+            # del value['FIPS']
+            data[key] = rows
+            # print(row['FIPS'], row['Admin2'], row['Province_State'], row['Deaths'])
+    #return dat
+    # return jsonData
     with open(tmpjson,'w', newline='') as jsonfile:
-        jsonfile.write(jsonData)
+        jsonfile.write(json.dumps(data,indent=4,sort_keys=True))
+    return data
     #df = pd.DataFrame(jsonData,columns=['Deaths', 'Province_State', 'Admin2'])
     #print(df)
-    return pd.read_csv(tmpcsv)
+    # return pd.read_csv(tmpcsv)
 
 if __name__ == '__main__':
     today = date.today()
     yesterday = format(today-timedelta(days=1),"%m-%d-%Y")
-    df = daily_report(yesterday)
+    #df = daily_report(yesterday)
+    us_df = daily_report_us(yesterday)
     
--- server/python/page.py
+++ server/python/page.py
@@ -1,50 +1,40 @@
 #!/usr/bin/env python
 # from lxml import etree
+import itertools
 from django.template import Template, Context
 from django.conf import settings
+import matplotlib.pyplot as plt
 from datetime import date, timedelta
 import pandas as pd
-from get_data import daily_report
+from get_data import daily_report,daily_report_us
 import django
+import numpy as np
 
-settings.configure()
-django.setup()
-template = """<html>
-<head>
-<title>Template {{ target_date }}
-</title>
-</head>
-<body>
-<table>
-<thead>
-{% for k in data.keys %}
-<th>{{k}}</th>
-{% endfor %}
-</thead>
-<tbody>
-{% for k,v in data.items %}
-<tr>
-<th>{{v}}</th>
-<td>{{v}}</td>
-</tr>
-</tbody>
-{% endfor %}
-
-
-</table>
-</body>
-</html>
-"""
-def doRender(dat):
-    # c = Context()
-    return template.render(Context(dat))
+# def doRender(dat):
+#     # c = Context()
+#     return template.render(Context(dat))
 
 # def main():
+plt.style.use('classic')
 today = date.today()
+# for i in range(7):
+#     try:
+#         target_date = format(today-timedelta(days=i),"%m-%d-%Y")
+#         daily = daily_report(target_date)
+        
+#     except Exception as e:
+#         print(e.args)
+#         if ('404' in e.args):
+#             continue
+#         else:
+#             break
+#     else:
+#        break
 for i in range(7):
     try:
         target_date = format(today-timedelta(days=i),"%m-%d-%Y")
-        daily = daily_report(target_date)
+        daily_us = daily_report_us(target_date)
+        
     except Exception as e:
         print(e.args)
         if ('404' in e.args):
@@ -54,8 +44,70 @@ for i in range(7):
     else:
         break
 
-usDaily = daily.query('Country_Region == "US"')
-di = pd.Index(daily.FIPS)
 
-df = pd.DataFrame(usDaily.filter(['FIPS','Admin2','Province_State','Confirmed','Deaths','Case_Fatality_Ratio']).sort_values('Confirmed', ascending=False))
-print(df)
+
+def label_bars(ax, heights, rects):
+    """Attach a text label on top of each bar."""
+    for height, rect in zip(heights, rects):
+        ax.annotate(f'{height}',
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 4),  # 4 points vertical offset.
+                    textcoords='offset points',
+                    ha='center', va='bottom')
+# daily = daily_report(target_date)
+#usDaily = daily.query('Country_Region == "US"')
+# di = pd.Index(daily.FIPS)
+print(daily_us)
+df = daily_us.sort_values('Confirmed', na_position='last', ascending=False)
+# df = daily_us.filter(['FIPS','Admin2','Province_State','Confirmed','Deaths','Case_Fatality_Ratio']).sort_values('Confirmed', ascending=False))
+# print(df)
+group_labels = ['Confirmed', 'Deaths', 'Case_Fatality_Ratio']
+xlabels = df['Province_State']
+# values = df.filter(xlabels).as_array()
+k = df.filter(group_labels)
+# k = list(map(lambda t: t[1], df.filter(xlabels).values()))
+values = np.asarray(k).transpose() # [ *itertools.chain(k) ])
+deaths = df['Deaths']
+confirmed = df['Confirmed']
+fig, ax = plt.subplots(nrows=3)
+
+x = np.arange(values.shape[1])
+# plt.plot(kind="bar")
+# plt.figure();
+for i in range(len(values)):
+    ax[i].set_xticks(x)
+    ax[i].set_xticklabels(xlabels,rotation=90)
+    dimw = 0.75
+    #for i in range(len(confirmed)):
+    #    y = confirmed[i]
+    # style = {'fill': False} if i == 0 else {'edgecolor': 'black'}
+    b = ax[i].bar(x + i * dimw, values[i], dimw, bottom=0.0001)
+    # label_bars(ax[i], values[i], b)
+    # spacing = 0.2 # spacing between hat groups
+    # width = (1 - spacing) / values.shape[0]
+    # heights0 = values[0]
+    # for i, (heights, group_label) in enumerate(zip(values, group_labels)):
+    #     print(group_label,heights)
+    #     style = {'fill': False} if i == 0 else {'edgecolor': 'black'}
+    #     rects = ax.bar( x - spacing/2 + i * width, heights, width, bottom=0, label=group_label, align='edge', **style)
+    #     label_bars(heights,rects)
+    # x = np.linspace(0, len(df)+1, len(df))
+    # fig = plt.figure()
+    # ax[i].legend()
+
+
+#b2 = ax[1].bar(x + i * dimw, deaths, dimw, bottom=0.0001)
+
+
+# fig.tight_layout()
+# ax2 = fig.add_subplot(211)
+# ax2.set_xticks(x)
+# ax2.set_xticklabels(xlabels,rotation=90)
+# line = ax2.plot(x,df['Case_Fatality_Ratio'])
+plt.show()
+#ax1 = fig.add_subplot(211) 
+#ax1.set_xlabel(df[''])
+#ax1.bar(x, df['Confirmed'])
+#ax1.bar(x, df['Deaths'])
+
+# plt.show()
